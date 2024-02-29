fa2188d2988016dd0e2e955c059bbfc5c222864f
DCRIl <133270010+DCRIl@users.noreply.github.com>
2024-02-29 19:37:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
+from forms.user import RegisterForm
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    print(jobs)
+    return render_template("index.html", jobs=jobs)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+def main():
+    db_session.global_init("db/colonists.db")
+    # db_sess = db_session.create_session()
+    # job = Jobs()
+    # job.team_leader = 2
+    # job.job = 'Сидеть на попе ровно'
+    # job.work_size = 1000
+    # job.collaborators = 'all users'
+    # job.is_finished = False
+    # db_sess.add(job)
+    # db_sess.commit()
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
